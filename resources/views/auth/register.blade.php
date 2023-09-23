844eb7c83d90e14b23c00d6be5b4bd70d8c367dc
BONNV <ngovanbon2001@gmail.com>
2023-09-23 16:31:43 +0700

    cart

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,9 +5,9 @@
 <div class="breadcrumb-wrap">
     <div class="container-fluid">
         <ul class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item"><a href="#">Products</a></li>
-            <li class="breadcrumb-item active">Login & Register</li>
+            <li class="breadcrumb-item"><a href="{{ route('web.home') }}">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('web.product') }}">Products</a></li>
+            <li class="breadcrumb-item active">Register</li>
         </ul>
     </div>
 </div>
