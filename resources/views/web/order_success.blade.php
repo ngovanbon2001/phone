7334c0fa616fa9222e0514b05258870bb90c0725
BONNV <ngovanbon2001@gmail.com>
2023-09-23 21:26:06 +0700

    up

--- resources/views/web/order_success.blade.php
+++ resources/views/web/order_success.blade.php
@@ -1,26 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Page Title -->
-    <div class="section section-breadcrumbs">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1>Order Successful</h1>
-                </div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="error-page-wrapper">
+                <h1>Successful!</h1>
+                <p>Order Successful! @if(isset(auth()->user()->id)) <a href="{{ route('order.show', auth()->user()->id) }}">Click here</a> @endif</p>
             </div>
         </div>
     </div>
-
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="error-page-wrapper">
-                        <p>Order Successful! @if(isset(auth()->user()->id)) <a href="{{ route('order.show', auth()->user()->id) }}">Click here</a> @endif</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+</div>
 @endsection
