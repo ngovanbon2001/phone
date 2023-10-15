1512122cdad0ef534cba79d753989912ed7d2bb1
BONNV <ngovanbon2001@gmail.com>
2023-10-15 22:01:26 +0700

    up

--- resources/lang/en/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'title' => 'Permissions',
-    'add_role' => 'Add Permission',
-    'store' => 'Permission info added successfully!',
-    'update' => 'Permission info updated successfully!',
-];
--- resources/lang/en/roles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'title' => 'Roles',
-    'add_role' => 'Add Role',
-    'store' => 'Role info added successfully!',
-    'update' => 'Role info updated successfully!',
-];
