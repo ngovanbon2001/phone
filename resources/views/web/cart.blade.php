36f44520a078d4196df70ec8f8a96c7ef9b4fa0f
BONNV <ngovanbon2001@gmail.com>
2023-10-19 18:16:42 +0700

    up

--- resources/views/web/cart.blade.php
+++ resources/views/web/cart.blade.php
@@ -103,6 +103,7 @@
     const urlAddress = "{{ route('select-delivery') }}";
 
     function checkQuantity(input) {
+        console.log(input);
         var value = parseFloat(input.value);
         if (value < 1) {
             input.value = 1;
